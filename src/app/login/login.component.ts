01806fe1cd81eeaf25af22df6ab9a85c03fde99e
Elda20 <ademarfa28@gmail.com>
2022-11-11 13:17:57 +0700

    Angular

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit, Renderer2 } from '@angular/core';
 export class LoginComponent implements OnInit {
 
   constructor(private renderer: Renderer2) { 
-    this.renderer.addClass(document.body, "login-page");
+    this.renderer.addClass(document.body, "login-page")
   }
 
   ngOnInit(): void {
